0cc8bf3c0e0c3284427f3c16c27afa426b27572b
Vito <vitohartanto@mail.ugm.ac.id>
2024-02-13 17:24:28 +0700

    diagnostics page

--- src/pages/Diagnostics.jsx
+++ src/pages/Diagnostics.jsx
@@ -6,6 +6,21 @@ const Diagnostics = () => {
       <Sidebar />
       <div className="ml-12">
         <h1 className="text-2xl font-bold ml-5 pt-8">Diagnostics</h1>
+        {/* fuel system status */}
+        {/* engine rpm */}
+        {/* vehicle speed */}
+        {/* throttle position */}
+        {/* engine coolant temperature */}
+        {/* short term fuel trim */}
+        {/* long term fuel trim */}
+        {/* intake air temperature */}
+        {/* oxygen sensor bank 1 sensor 1 */}
+        {/* oxygen sensor bank 2 sensor 2 */}
+        {/* mass air flow */}
+        {/* catalyst temperature */}
+        {/* fuel type */}
+        {/* engine oil temperature */}
+        {/* intake manifold pressure */}
       </div>
     </div>
   );
