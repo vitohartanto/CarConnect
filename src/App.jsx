10d9afda78d95762e146e9c2e8ad49260f72fbe3
Vito <vitohartanto@mail.ugm.ac.id>
2024-03-17 11:04:41 +0700

    mencoba debugging

--- src/App.jsx
+++ src/App.jsx
@@ -15,35 +15,34 @@ const App = () => {
   const [variablesInObject, setVariablesInObject] = useState({});
   const [dtcResponse, setDtcResponse] = useState(null);
 
+  socket.on("data", (msg) => {
+    console.log("Ini msg");
+    console.log(msg);
+    let carData = JSON.parse(msg);
+    console.log("Ini carData");
+    console.log(carData);
+    setVariablesInObject((previousVariablesInObjects) => ({
+      // Update variablesInObject
+      ...previousVariablesInObjects,
+      v_fuelSystemStatus: carData["varFuelSystemStatus"],
+      v_engineRpm: parseInt(carData["varEngineRpm"]),
+      v_vehicleSpeed: parseInt(carData["varVehicleSpeed"]),
+      v_throttlePosition: parseInt(carData["varThrottlePosition"]),
+      v_engineCoolantTemperature: parseInt(
+        carData["varEngineCoolantTemperature"]
+      ),
+      v_shortTermFuelTrim: parseInt(carData["varShortTermFuelTrim"]),
+      v_longTermFuelTrim: parseInt(carData["varLongTermFuelTrim"]),
+      v_intakeAirTemperature: parseInt(carData["varIntakeAirTemperature"]),
+      v_catalystTemperature: parseInt(carData["varCatalystTemperature"]),
+      v_intakeManifoldPressure: parseInt(carData["varIntakeManifoldPressure"]),
+    }));
+    console.log("Ini carData yang seharusnya salah");
+    console.log(carData);
+  });
+
   useEffect(() => {
     console.log("Client mulai menerima data");
-    socket.on("data", (msg) => {
-      console.log("Ini msg");
-      console.log(msg);
-      let carData = JSON.parse(msg);
-      console.log("Ini carData");
-      console.log(carData);
-      setVariablesInObject((previousVariablesInObjects) => ({
-        // Update variablesInObject
-        ...previousVariablesInObjects,
-        v_fuelSystemStatus: carData["varFuelSystemStatus"],
-        v_engineRpm: parseInt(carData["varEngineRpm"]),
-        v_vehicleSpeed: parseInt(carData["varVehicleSpeed"]),
-        v_throttlePosition: parseInt(carData["varThrottlePosition"]),
-        v_engineCoolantTemperature: parseInt(
-          carData["varEngineCoolantTemperature"]
-        ),
-        v_shortTermFuelTrim: parseInt(carData["varShortTermFuelTrim"]),
-        v_longTermFuelTrim: parseInt(carData["varLongTermFuelTrim"]),
-        v_intakeAirTemperature: parseInt(carData["varIntakeAirTemperature"]),
-        v_catalystTemperature: parseInt(carData["varCatalystTemperature"]),
-        v_intakeManifoldPressure: parseInt(
-          carData["varIntakeManifoldPressure"]
-        ),
-      }));
-      console.log("Ini carData yang seharusnya salah");
-      console.log(carData);
-    });
 
     socket.on("dtcData", (faultCodes) => {
       setDtcResponse(faultCodes);
