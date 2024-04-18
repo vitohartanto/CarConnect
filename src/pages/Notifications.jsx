8551c2bcf793f35d50e309ff66625c05cd59eb80
Vito <vitohartanto@mail.ugm.ac.id>
2024-04-18 08:58:11 +0700

    notifications responsive

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,5 +1,5 @@
 import Sidebar from "../components/Sidebar";
-import { FaWrench } from "react-icons/fa";
+import { FaWrench, FaHeart } from "react-icons/fa";
 
 const Notifications = () => {
   return (
@@ -9,38 +9,47 @@ const Notifications = () => {
         <h1 className="pt-8 ml-5 text-2xl font-bold md:text-3xl xl:text-4xl sm:ml-10">
           Notifications
         </h1>
-        <h1 className="mt-6 ml-5 text-2xl font-bold md:text-3xl xl:text-4xl">
+        <h1 className="mt-6 ml-5 text-2xl font-bold sm:ml-10 md:text-3xl xl:text-4xl">
           Example:
         </h1>
-        <div className="mx-4 mt-4">
-          <div className="shadow-[0px_0px_30px_0px_rgba(0,0,0,0.8)] px-4 py-4 rounded-2xl flex justify-center items-center">
-            <FaWrench className="text-[#233163] text-3xl w-2/5 mr-2" />
+        <div className="mx-4 sm:mx-10 ">
+          <div className="mt-4 lg:mt-6 shadow-[0px_0px_30px_0px_rgba(0,0,0,0.8)] px-4 py-4 md:py-6 rounded-2xl flex justify-start items-center">
+            <FaWrench className="text-[#233163] text-3xl md:text-4xl lg:text-5xl w-16 mr-2 lg:mx-6" />
             <div>
-              <h2 className="text-sm font-medium">
+              <h2 className="text-sm font-medium md:text-base lg:text-lg">
                 Engine Coolant Temperature is out of optimal range
               </h2>
-              <p className="mt-2 text-xs">19 April 2024 08:35:21</p>
+              <p className="mt-2 text-xs md:text-sm lg:text-base">
+                19 April 2024 08:35:21
+              </p>
             </div>
           </div>
-          <div className="shadow-[0px_0px_30px_0px_rgba(0,0,0,0.8)] px-4 py-4 rounded-2xl flex justify-center items-center">
-            <FaWrench className="text-[#233163] text-3xl w-2/5 mr-2" />
+          <div className="mt-4 lg:mt-6 shadow-[0px_0px_30px_0px_rgba(0,0,0,0.8)] px-4 py-4 md:py-6 rounded-2xl flex justify-start items-center">
+            <FaHeart className="text-[#233163] text-3xl md:text-4xl lg:text-5xl w-16 mr-2 lg:mx-6" />
             <div>
-              <h2 className="text-sm font-medium">
+              <h2 className="text-sm font-medium md:text-base lg:text-lg">
                 Short Term Fuel Trim problem has been fixed
               </h2>
-              <p className="mt-2 text-xs">17 April 2024 12:32:12</p>
+              <p className="mt-2 text-xs md:text-sm lg:text-base">
+                17 April 2024 12:32:12
+              </p>
             </div>
           </div>
-          <div className="shadow-[0px_0px_30px_0px_rgba(0,0,0,0.8)] px-4 py-4 rounded-2xl flex justify-center items-center">
-            <FaWrench className="text-[#233163] text-3xl w-2/5 mr-2" />
+          <div className="mt-4 lg:mt-6 shadow-[0px_0px_30px_0px_rgba(0,0,0,0.8)] px-4 py-4 md:py-6 rounded-2xl flex justify-start items-center">
+            <FaWrench className="text-[#233163] text-3xl md:text-4xl lg:text-5xl w-16 mr-2 lg:mx-6" />
             <div>
-              <h2 className="text-sm font-medium">
+              <h2 className="text-sm font-medium md:text-base lg:text-lg">
                 Short Term Fuel Trim is out of optimal range
               </h2>
-              <p className="mt-2 text-xs">19 April 2024 08:35:21</p>
+              <p className="mt-2 text-xs md:text-sm lg:text-base">
+                17 April 2024 08:35:21
+              </p>
             </div>
           </div>
         </div>
+        <h1 className="mt-6 ml-5 text-2xl font-bold lg:mt-10 sm:ml-10 md:text-3xl xl:text-4xl">
+          Real Notifications:
+        </h1>
       </div>
     </div>
   );
