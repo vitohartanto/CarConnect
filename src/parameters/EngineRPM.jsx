5e13cca4e3b3305db3fd7850b1d52f475c1ba982
Vito <vitohartanto@mail.ugm.ac.id>
2024-04-21 15:21:21 +0700

    basic engine parameters

--- src/parameters/EngineRPM.jsx
+++ src/parameters/EngineRPM.jsx
@@ -5,13 +5,13 @@ const EngineRPM = ({ carId, children }) => {
   const variablesInObject = useContext(AppContext); // Destructuring variablesInObject from the context object
 
   return (
-    <div className=" my-4 backdrop-blur-[2px] border-[1px_solid_rgba(255,255,255,0.18)] shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] rounded-[18px] bg-[rgba(25,25,25,0.90)] relative  flex flex-col justify-start">
+    <div className=" my-4 backdrop-blur-[2px] border-[1px_solid_rgba(255,255,255,0.18)] shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] rounded-[18px] bg-[rgba(25,25,25,0.90)] relative min-h-[180px] max-h-[220px] max-w-[320px] flex flex-col justify-center">
       {children}
-      <h1 className="absolute top-[50px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-base font-semibold text-center md:text-lg lg:text-xl">
+      <h1 className="pb-[20px] text-base font-semibold text-center md:text-lg lg:text-xl">
         Engine RPM
       </h1>
 
-      <h1 className="mt-[100px] pb-[20px] py-2 text-5xl font-semibold text-center">
+      <h1 className="pt-[20px] text-5xl font-semibold text-center">
         {variablesInObject[carId]?.v_engineRpm}650 RPM
       </h1>
     </div>
