9044932a1c56539e3407df67b36a56690fb4a75e
Vito <vitohartanto@mail.ugm.ac.id>
2024-04-22 09:04:42 +0700

    registering car now has been 2 inputs

--- src/pages/RegisteredCars.jsx
+++ src/pages/RegisteredCars.jsx
@@ -118,28 +118,47 @@ const RegisteredCars = () => {
   };
 
   const addCarLicensePlate = async () => {
-    const { value: license_plate } = await Swal.fire({
-      title: "Enter Car's License Plate",
-      inputPlaceholder: "e.g. B 1234 VH",
-      input: "text",
+    const { value: plate_brand } = await Swal.fire({
+      title: "Enter Car's License Plate and Car's Brand",
+      html: `
+    <input id="swal-input1" class="swal2-input placeholder-white text-base min-w-[230px]" placeholder="e.g. B 1234 VH">
+    <input id="swal-input2" class="swal2-input placeholder-white text-base min-w-[230px]" placeholder="e.g. Toyota, Honda, Mazda">
+  `,
+      focusConfirm: false,
+
+      preConfirm: (plate_brand) => {
+        if (!plate_brand[0] || !plate_brand[1]) {
+          Swal.fire({
+            color: "#fff",
+            background: "rgba(25,25,25,0.90)",
+            backdrop: `rgba(7,193,250,0.1)`,
+
+            confirmButtonColor: "#16db3d",
+
+            icon: "error",
+            title: "Error",
+            text: "You need to complete both inputs!",
+          });
+        }
+        return JSON.stringify([
+          document.getElementById("swal-input1").value,
+          document.getElementById("swal-input2").value,
+        ]);
+      },
+
       color: "#fff",
       background: "rgba(25,25,25,0.90)",
       backdrop: `rgba(7,193,250,0.1)`,
       cancelButtonColor: "#d33",
       confirmButtonColor: "#16db3d",
       showCancelButton: true,
-      inputValidator: (value) => {
-        if (!value) {
-          return "You need to write something!";
-        }
-      },
     });
 
-    if (license_plate) {
+    if (plate_brand) {
       try {
         await carsCollection.insertOne({
           user_id: hyperbase.user._id,
-          license_plate,
+          plate_brand,
         });
         await fetchAllCars();
       } catch (err) {
@@ -275,53 +294,61 @@ const RegisteredCars = () => {
         </Fade>
 
         <div className="flex flex-col flex-grow px-5 mb-6 md:px-12 min-[600px]:flex-row min-[600px]:flex-wrap min-[600px]:justify-start">
-          {cars.map((car) => (
-            <a
-              key={car._id}
-              href={`/app/${car._id}`}
-              className="mx-2 px-8 py-6 lg:py-8 mt-6 flex flex-col max-w-[300px] min-[600px]:w-[300px] font-medium backdrop-blur-[2px] border-[1px_solid_rgba(255,255,255,0.18)] shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] rounded-[18px] bg-[rgba(255,255,255,0.90)]"
-            >
-              <div className="flex items-center justify-between mb-4">
-                <div>
-                  <h1 className="text-[#191919] min-[600px]:text-xl xl:text-2xl">
-                    {car.license_plate}
-                  </h1>
-                  <h1 className="text-[#191919]">Mazda</h1>
-                </div>
-
-                <div>
-                  <FaCircle className="text-[#20F95D]" />
-                </div>
-              </div>
-
-              <div className="flex items-center justify-center">
-                <a href={`/app/${car._id}/notifications`} className="relative">
-                  <FaBell className="text-[#191919] text-lg w-10 h-10 p-2 rounded-full backdrop-blur-[2px] border-[1px_solid_rgba(255,255,255,0.18)] shadow-[0_8px_32px_0_rgba(31,38,135,0.37)]  bg-[rgba(255,255,255,0.90)]" />
-                  <div className="absolute top-1 left-5 text-[#191919] w-5 h-5 rounded-full backdrop-blur-[2px] border-[1px_solid_rgba(255,255,255,0.18)] shadow-[0_8px_32px_0_rgba(31,38,135,0.37)]  bg-[rgba(255,0,0,0.90)]">
-                    <p className="absolute top-[-1px] left-[6px] text-sm">3</p>
+          {cars.map((car) => {
+            let parsedPlateBrand = JSON.parse(car.plate_brand);
+            return (
+              <a
+                key={car._id}
+                href={`/app/${car._id}`}
+                className="mx-2 px-8 py-6 lg:py-8 mt-6 flex flex-col max-w-[300px] min-[600px]:w-[300px] font-medium backdrop-blur-[2px] border-[1px_solid_rgba(255,255,255,0.18)] shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] rounded-[18px] bg-[rgba(255,255,255,0.90)]"
+              >
+                <div className="flex items-center justify-between mb-4">
+                  <div>
+                    <h1 className="text-[#191919] min-[600px]:text-xl xl:text-2xl">
+                      {parsedPlateBrand[1]}
+                    </h1>
+                    <h1 className="text-[#191919]">{parsedPlateBrand[1]}</h1>
                   </div>
-                </a>
-                <button
-                  className="ml-4 sm:text-xl lg:text-2xl w-10 h-10 rounded-full backdrop-blur-[2px] border-[1px_solid_rgba(255,255,255,0.18)] shadow-[0_8px_32px_0_rgba(31,38,135,0.37)]  bg-[rgba(255,255,255,0.90)]"
-                  onClick={(e) => editCarLicensePlate(e, car._id)}
-                >
-                  <FontAwesomeIcon
-                    icon={faPenToSquare}
-                    style={{ color: "#191919" }}
-                  />
-                </button>
-                <button
-                  className="ml-4 sm:text-xl lg:text-2xl w-10 h-10 rounded-full backdrop-blur-[2px] border-[1px_solid_rgba(255,255,255,0.18)] shadow-[0_8px_32px_0_rgba(31,38,135,0.37)]  bg-[rgba(255,255,255,0.90)]"
-                  onClick={(e) => deleteCarLicensePlate(e, car._id)}
-                >
-                  <FontAwesomeIcon
-                    icon={faTrash}
-                    style={{ color: "#191919" }}
-                  />
-                </button>
-              </div>
-            </a>
-          ))}
+
+                  <div>
+                    <FaCircle className="text-[#20F95D]" />
+                  </div>
+                </div>
+
+                <div className="flex items-center justify-center">
+                  <a
+                    href={`/app/${car._id}/notifications`}
+                    className="relative"
+                  >
+                    <FaBell className="text-[#191919] text-lg w-10 h-10 p-2 rounded-full backdrop-blur-[2px] border-[1px_solid_rgba(255,255,255,0.18)] shadow-[0_8px_32px_0_rgba(31,38,135,0.37)]  bg-[rgba(255,255,255,0.90)]" />
+                    <div className="absolute top-1 left-5 text-[#191919] w-5 h-5 rounded-full backdrop-blur-[2px] border-[1px_solid_rgba(255,255,255,0.18)] shadow-[0_8px_32px_0_rgba(31,38,135,0.37)]  bg-[rgba(255,0,0,0.90)]">
+                      <p className="absolute top-[-1px] left-[6px] text-sm">
+                        3
+                      </p>
+                    </div>
+                  </a>
+                  <button
+                    className="ml-4 sm:text-xl lg:text-2xl w-10 h-10 rounded-full backdrop-blur-[2px] border-[1px_solid_rgba(255,255,255,0.18)] shadow-[0_8px_32px_0_rgba(31,38,135,0.37)]  bg-[rgba(255,255,255,0.90)]"
+                    onClick={(e) => editCarLicensePlate(e, car._id)}
+                  >
+                    <FontAwesomeIcon
+                      icon={faPenToSquare}
+                      style={{ color: "#191919" }}
+                    />
+                  </button>
+                  <button
+                    className="ml-4 sm:text-xl lg:text-2xl w-10 h-10 rounded-full backdrop-blur-[2px] border-[1px_solid_rgba(255,255,255,0.18)] shadow-[0_8px_32px_0_rgba(31,38,135,0.37)]  bg-[rgba(255,255,255,0.90)]"
+                    onClick={(e) => deleteCarLicensePlate(e, car._id)}
+                  >
+                    <FontAwesomeIcon
+                      icon={faTrash}
+                      style={{ color: "#191919" }}
+                    />
+                  </button>
+                </div>
+              </a>
+            );
+          })}
         </div>
       </div>
     </div>
