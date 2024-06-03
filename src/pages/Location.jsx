168a9445056de59a14ea59063405fb6d4fc3d6f2
Vito <vitohartanto@mail.ugm.ac.id>
2024-06-03 11:18:47 +0700

    feat: adding gps feature

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -13,6 +13,10 @@ import 'leaflet/dist/leaflet.css';
 import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2/dist/sweetalert2.js';
 import 'sweetalert2/src/sweetalert2.scss';
+import { useState } from 'react';
+import collections from '../utils/hyperbase/hyperbaseCollections.json';
+import { HyperbaseContext } from '../App';
+import { useContext } from 'react';
 
 const DynamicMap = ({ bounds }) => {
   const map = useMap();
@@ -27,36 +31,195 @@ const DynamicMap = ({ bounds }) => {
 };
 
 const Location = () => {
+  const hyperbase = useContext(HyperbaseContext);
+  const [carsCollection, setCarsCollection] = useState();
+  const [obdCollection, setObdCollection] = useState();
+  const [cars, setCars] = useState([]);
+  const [obd, setObd] = useState([]);
   const navigate = useNavigate();
-  const publicFacilities = [
+
+  useEffect(() => {
+    if (hyperbase.isLoading || !hyperbase.isSignedIn) return;
+
+    let unsubscribe;
+
+    (async () => {
+      try {
+        const carCollection = await hyperbase.setCollection(collections.cars);
+        unsubscribe = subscribe(carCollection);
+        console.log(carCollection);
+        setCarsCollection(carCollection);
+      } catch (err) {
+        alert(`${err.status}\n${err.message}`);
+      }
+    })();
+
+    return () => {
+      if (unsubscribe) unsubscribe();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [hyperbase, hyperbase.isLoading]);
+
+  useEffect(() => {
+    if (!carsCollection) return;
+    fetchAllCars();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [carsCollection]);
+
+  const fetchAllCars = async () => {
+    try {
+      const cars = await carsCollection.findMany({
+        orders: [
+          {
+            field: '_id',
+            kind: 'asc',
+          },
+        ],
+      });
+      console.log(cars.data);
+      setCars(cars.data);
+    } catch (err) {
+      alert(`${err.status}\n${err.message}`);
+    }
+  };
+
+  const subscribe = (carsCollection) => {
+    carsCollection.subscribe({
+      onOpenCallback: (e) => {
+        console.log('Subscribe cars status open:', e);
+      },
+      onErrorCallback: (e) => {
+        console.log('Subscribe cars status error:', e);
+      },
+      onCloseCallback: (e) => {
+        console.log('Subscribe cars status close:', e);
+        if (e.status !== 1000) {
+          setTimeout(() => {
+            subscribe(carsCollection);
+          }, 5000);
+        }
+      },
+      onMessageCallback: (e) => {
+        console.log('Subscribe cars status message:', e);
+      },
+    });
+
+    return () => carsCollection.unsubscribe(1000);
+  };
+
+  // OBD COLLECTION
+
+  useEffect(() => {
+    if (hyperbase.isLoading || !hyperbase.isSignedIn) return;
+
+    let unsubscribe;
+
+    (async () => {
+      try {
+        const obdDataCollection = await hyperbase.setCollection(
+          collections.obd_data
+        );
+        unsubscribe = subscribeObd(obdDataCollection);
+        console.log(obdDataCollection);
+        setObdCollection(obdDataCollection);
+      } catch (err) {
+        alert(`${err.status}\n${err.message}`);
+      }
+    })();
+
+    return () => {
+      if (unsubscribe) unsubscribe();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [hyperbase, hyperbase.isLoading]);
+
+  useEffect(() => {
+    if (!obdCollection) return;
+    fetchAllLocations();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [obdCollection]);
+
+  const fetchAllLocations = async (car_id) => {
+    try {
+      const obdDatas = await obdCollection.findMany({
+        fields: ['latitude', 'longitude', 'car_id', '_id'],
+        filters: [{ field: 'car_id', op: '=', value: car_id }],
+        orders: [{ field: '_id', kind: 'asc' }],
+        limit: 1,
+      });
+      console.log(obdDatas.data);
+      setObd(obdDatas.data);
+    } catch (err) {
+      alert(`${err.status}\n${err.message}`);
+    }
+  };
+
+  const subscribeObd = (obdCollection) => {
+    obdCollection.subscribe({
+      onOpenCallback: (e) => {
+        console.log('Subscribe obd status open:', e);
+      },
+      onErrorCallback: (e) => {
+        console.log('Subscribe obd status error:', e);
+      },
+      onCloseCallback: (e) => {
+        console.log('Subscribe obd status close:', e);
+        if (e.status !== 1000) {
+          setTimeout(() => {
+            subscribeObd(obdCollection);
+          }, 5000);
+        }
+      },
+      onMessageCallback: (e) => {
+        console.log('Subscribe obd status message:', e);
+      },
+    });
+
+    return () => obdCollection.unsubscribe(1000);
+  };
+
+  const fetchLocationsForAllCars = async () => {
+    for (const car of cars) {
+      await fetchAllLocations(car._id);
+    }
+  };
+
+  useEffect(() => {
+    fetchLocationsForAllCars();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [obdCollection]);
+
+  const carsLocation = [
     {
       _id: '123',
-      name: 'town hall',
-      typology: 'office',
+      car_plate: 'AB 1911 TE',
+      car_brand: 'Mazda Sedan 2',
       coordinates: [-7.698259, 110.433832],
     },
     {
       _id: '234',
-      name: 'town hall 2',
-      typology: 'sports',
+      car_plate: 'AD 1933 YA',
+      car_brand: 'Mazda CX-5',
       coordinates: [-7.696242, 110.435832],
     },
     {
       _id: '456789',
-      name: 'town hall',
-      typology: 'office',
+      car_plate: 'AB 1345 UB',
+      car_brand: 'Honda Brio',
       coordinates: [-7.698226, 110.436832],
     },
   ];
 
-  let uniquePublicFacilities = [
+  let uniqueCars = [
     ...new Map(
-      publicFacilities.map((item) => [
-        item.typology,
+      carsLocation.map((item) => [
+        item.car_plate,
         {
           ...item,
-          coordinates: publicFacilities
-            .filter(({ typology }) => typology === item.typology)
+          coordinates: carsLocation
+            .filter(({ car_plate }) => car_plate === item.car_plate)
             .map(({ coordinates }) => coordinates),
         },
       ])
@@ -64,7 +227,7 @@ const Location = () => {
   ];
 
   // Collect all coordinates for setting bounds
-  const bounds = publicFacilities.map((facility) => facility.coordinates);
+  const bounds = carsLocation.map((carLocation) => carLocation.coordinates);
 
   const backToRegisteredCarsHandler = async (event) => {
     // Assuming event is passed from an event listener
@@ -104,7 +267,7 @@ const Location = () => {
           </div>
         </button>
       </div>
-      <div className="ml-12 sm:ml-16 sm:ml-20">
+      <div className="ml-12 sm:ml-16 lg:ml-20">
         <MapContainer
           center={[51.51, -0.09]}
           zoom={8}
@@ -118,18 +281,18 @@ const Location = () => {
           <DynamicMap bounds={bounds} />
 
           <LayersControl position="topright">
-            {uniquePublicFacilities.map((facility) => (
+            {uniqueCars.map((uniqueCar) => (
               <LayersControl.Overlay
-                key={facility.typology}
+                key={uniqueCar.car_plate}
                 checked
-                name={facility.typology}
+                name={uniqueCar.car_plate}
               >
                 <FeatureGroup>
-                  {facility.coordinates.map((latlng, index) => (
+                  {uniqueCar.coordinates.map((latlng, index) => (
                     <Marker key={index} position={latlng}>
                       <Popup>
-                        AB 1911 TE <br />
-                        Mazda Sedan 2 <br />
+                        {uniqueCar.car_plate} <br />
+                        {uniqueCar.car_brand} <br />
                         Coordinates: {latlng[0]}, {latlng[1]}
                       </Popup>
                     </Marker>
