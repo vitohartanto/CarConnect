6bfbeefe2d40106a247e70840326d2032737b7ad
Vito <vitohartanto@mail.ugm.ac.id>
2024-06-07 07:19:15 +0700

    fix: fixing fuel system status

--- src/parameters/FuelSystemStatus.jsx
+++ src/parameters/FuelSystemStatus.jsx
@@ -8,12 +8,13 @@ const FuelSystemStatus = ({ carId, children }) => {
   return (
     <div className=" my-4 backdrop-blur-[2px] border-[1px_solid_rgba(255,255,255,0.18)] shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] rounded-[18px] bg-[rgba(25,25,25,0.90)] relative min-h-[180px] max-h-[220px] max-w-[320px] flex flex-col justify-center">
       {children}
-      <h1 className="px-10 pt-3 pb-[20px] text-base font-semibold text-center md:text-lg lg:text-xl">
+      <h1 className="px-10 pt-0 pb-0 text-base font-semibold text-center md:text-lg lg:text-xl">
         Fuel System Status
       </h1>
       <h1 className="px-2 pb-3 pt-[20px] text-base font-semibold text-center">
         {variablesInObject[carId]?.v_fuelSystemStatus}
-        Closed loop, using oxygen sensor feedback to determine fuel mix
+
+        {/* Closed loop, using oxygen sensor feedback to determine fuel mix */}
       </h1>
     </div>
   );
